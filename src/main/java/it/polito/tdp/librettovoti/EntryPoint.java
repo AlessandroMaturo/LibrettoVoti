5a5e4bb17e21f147b642796779ad9a780d1f12d2
User <User@DESKTOP-NFJOD61.Home>
2022-03-21 18:01:46 +0100

    Versione 1

--- src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
+++ src/main/java/it/polito/tdp/librettovoti/EntryPoint.java
@@ -5,16 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Libretto;
 
 
 public class EntryPoint extends Application {
 
+	//TUTTO QUESTO E' PER EVITARE CHE CONTROLLER FACCIA LA NEW DEL MODEL
+	
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+     // Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+        
+        //Al posto di creare uno statico cne creo uno in cui gli dico quale scena caricare nel costruttore e 
+        //poi dopo gli dico la scena da caricare
+        
+        FXMLController controller = loader.getController();
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
+        
+        Libretto model = new Libretto();
+        controller.setModel(model);
         
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,101 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.librettovoti;
 
+
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.Libretto;
+import model.Voto;
 
-public class FXMLController implements Initializable {
+public class FXMLController {
+	
+	private Libretto model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="bntAggiungi"
+    private Button bntAggiungi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbPunti"
+    private ComboBox<Integer> cmbPunti; // Value injected by FXMLLoader
     
+    @FXML // fx:id="lblErrore"
+    private Label lblErrore; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtNome"
+    private TextField txtNome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtVoto"
+    private TextArea txtVoto; // Value injected by FXMLLoader
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleNuovoVoto(ActionEvent event) {
+    	
+    	//1. Acquisizione e controllo dati
+    	String nome = txtNome.getText();
+    	Integer punti = cmbPunti.getValue();
+    	
+    	//controlli di validità
+    	if(nome.equals("") || punti == null) {
+    		//errore, non posso eseguire l'operazione 
+    		txtVoto.setText("ERRORE: occorre inserire nome e voto\n");
+    		return;
+    	}
+    	
+    	//2. esecuzione dell'operazione (== chiedere al Model di farla)
+    	boolean ok=model.add(new Voto(nome, punti));
+    	
+    	//3. Visualizzazione/aggiornamento del risultato
+    	if(ok) {
+        	List<Voto> voti=model.getVoti();
+        	txtVoto.clear();
+        	txtVoto.appendText("Hai superato "+ voti.size()+ " esami\n");
+        	for(Voto vi: voti) {
+        		txtVoto.appendText(vi.toString()+"\n");
+        	}
+        	txtNome.clear();
+        	cmbPunti.setValue(null);
+        	lblErrore.setText("");
+    	} else {
+    		lblErrore.setText("ERRORE");
+    	}
+    	
+    	//Adesso è controller che decide come visualizzare i dati
+    	
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model = model;
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert bntAggiungi != null : "fx:id=\"bntAggiungi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert cmbPunti != null : "fx:id=\"cmbPunti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        //assert lblErrore != null : "fx:id=\"lblErrore\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        cmbPunti.getItems().clear();
+        for(int p=18;p<=30;p++) {
+        	cmbPunti.getItems().add(p);
+        }
+
+    }
+
 }
